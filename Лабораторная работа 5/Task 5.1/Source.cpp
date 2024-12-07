93702564a041f8b9f01cb8fd6e8f2eb116f7e147
Senya <arspetrov545@gmail.com>
2024-12-07 11:57:12 +0700

    строчка Получено

--- Лабораторная работа 5/Task 5.1/Source.cpp
+++ Лабораторная работа 5/Task 5.1/Source.cpp
@@ -21,5 +21,5 @@ int main() {
     std::cout << "Y в десятичной системе" << std::dec << Y << std::endl;
     std::cout << "Y в восьмеричной системе: " << std::oct << Y << std::endl;
     std::cout << "Y в шестнадцатеричной системе: " << std::hex << Y << std::endl;
-    
+    std::cout << "Получено" << std::endl;
 }
